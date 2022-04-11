a881a32aa98990795d7e4ed46ecdea4334951828
asadulR <asad1252001@gmail.com.com>
2022-04-11 14:04:48 +0600

    simple setup

---
+++ src/Pages/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section>
+            <h2>This is about us.</h2>
+        </section>
+    );
+};
+
+export default About;
