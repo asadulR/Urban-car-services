dc183b78dab37572dad6bae307d6564b3bc76721
asadulR <asad1252001@gmail.com.com>
2022-04-13 02:29:35 +0600

    environment variable setup

--- src/Pages/Login/Signup/Signup.js
+++ src/Pages/Login/Signup/Signup.js
@@ -1,25 +1,50 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import '../Login/Login.css';
+
+
 const Signup = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
     const confirmPasswordRef = useRef('');
 
+    const navigate = useNavigate();
+
+    const handleSignup = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const confirmPassword = event.target.confirmPassword.value;
+
+        console.log(name, email, password, confirmPassword)
+
+    }
+
+
+
+
+
+
+
+    const navigateLogin = () => {
+        navigate('/login')
+    }
 
     
     return (
         <div className='container'>
             <div className='form-container'>
                 <h2 className='text-center mb-5 fw-bold login-title'>Please Signup</h2>
-                <Form >
+                <Form onSubmit={handleSignup}>
                     <Form.Group className="mb-3" controlId="formName">
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" placeholder="Your name" />
+                        <Form.Control type="text" name='name' placeholder="Your name" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control ref={emailRef} required type="email" placeholder="Enter email" />
+                        <Form.Control ref={emailRef} name='email' required type="email" placeholder="Enter email" />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                         </Form.Text>
@@ -27,18 +52,19 @@ const Signup = () => {
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control ref={passwordRef} required type="password" placeholder="Password" />
+                        <Form.Control ref={passwordRef} required name='password' type="password" placeholder="Password" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formConfirmPassword">
                         <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control ref={confirmPasswordRef} required type="password" placeholder="Confirm Password" />
+                        <Form.Control ref={confirmPasswordRef} required name='confirmPassword' type="password" placeholder="Confirm Password" />
                     </Form.Group>
                     <div className='text-center mt-5'>
                         <Button className='login-btn py-2 fs-5' type="submit">
-                            Login
+                            Signup
                         </Button>
                     </div>
                 </Form>
+                <p className='mt-4'>Already have an account ? <span onClick={navigateLogin} className='signup-link'>Login</span></p>
             </div>
         </div>
     );
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPxhq6WOLmavZryRMQAWWCQsLbIgT3X20",
-  authDomain: "genius-car-services-78fb0.firebaseapp.com",
-  projectId: "genius-car-services-78fb0",
-  storageBucket: "genius-car-services-78fb0.appspot.com",
-  messagingSenderId: "317634314693",
-  appId: "1:317634314693:web:c8137817794caa9f900bac"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
