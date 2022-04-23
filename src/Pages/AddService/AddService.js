04a87f137636541461ebab57d49388fbe0b5fd86
asadulR <asad1252001@gmail.com.com>
2022-04-23 18:10:02 +0600

    service added by the user to database and get that in the UI

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -5,7 +5,24 @@ import { useForm } from "react-hook-form";
 const AddService = () => {
 
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data)
+        const url = `http://localhost:5000/service/`;
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(result => {
+            console.log(result);
+        })
+    
+    };
     return (
         <div className='w-75 mx-auto'>
             <h2 className='text-center my-5 fw-bold'>Please add a service </h2>
@@ -15,7 +32,7 @@ const AddService = () => {
                 <textarea className='mb-4 p-2' placeholder='Description' {...register("description")} />
                 <input className='mb-4 p-2' placeholder='Price' type="number" {...register("price")} />
                 <input className='mb-4 p-2' placeholder='Photo URL' type="text" {...register("img")} />
-                <input className='mb-4 py-2 fw-bold' type="submit" />
+                <input className='mb-4 py-2 fw-bold' type="submit" value='Add service'/>
             </form>
         </div>
     );
