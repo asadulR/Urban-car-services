17688c0ab5144e2f4b7ebf2c537782e95f33c091
asadulR <asad1252001@gmail.com.com>
2022-04-23 17:53:31 +0600

    add service form added

---
+++ src/Pages/AddService/AddService.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useForm } from "react-hook-form";
+
+
+const AddService = () => {
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+    return (
+        <div className='w-75 mx-auto'>
+            <h2 className='text-center my-5 fw-bold'>Please add a service </h2>
+
+            <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                <input className='mb-4 p-2' placeholder='Service name' {...register("name", { required: true, maxLength: 20 })} />
+                <textarea className='mb-4 p-2' placeholder='Description' {...register("description")} />
+                <input className='mb-4 p-2' placeholder='Price' type="number" {...register("price")} />
+                <input className='mb-4 p-2' placeholder='Photo URL' type="text" {...register("img")} />
+                <input className='mb-4 py-2 fw-bold' type="submit" />
+            </form>
+        </div>
+    );
+};
+
+export default AddService;
